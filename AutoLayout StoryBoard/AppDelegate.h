5d8e76d0101dc4e1a7ddc43726b3a0cb2ff5738f
RKing <rosalyn.kingsmill@gmail.com>
2016-05-10 14:34:20 -0400

    Initial Commit

---
+++ AutoLayout StoryBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayout StoryBoard
+//
+//  Created by Rosalyn Kingsmill on 2016-05-10.
+//  Copyright © 2016 Rosalyn Kingsmill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
